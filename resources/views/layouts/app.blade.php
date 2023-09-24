349f7dd3ca13ea44ac00421e8a819dffd556f5b6
Ankithirau008 <ankit.owlsy@gmail.com>
2023-09-24 16:38:45 +0530

    sss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+    <!-- CSRF Tokensss -->
+
+    
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
